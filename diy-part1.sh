420cdb540c9dd4d898ebb03d2587ce99c6148d1f
netswimer <gyf319@gmail.com>
2024-01-03 18:35:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+  echo 'src-git kenzok8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+  echo 'src-git kenzok8d https://github.com/kenzok8/small' >>feeds.conf.default
